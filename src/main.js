6900d650642737b2650dd9ffa945a3b5bfc84c12
Hijikata1869 <liv.knk@gmail.com>
2023-11-03 13:44:29 +0900

    純粋なjavascriptのみでTODOリストを作成

--- src/main.js
+++ src/main.js
@@ -11,8 +11,41 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    const completeTarget = completeButton.parentNode;
+    completeTarget.removeChild(completeButton);
+    completeTarget.removeChild(deleteButton);
+    completeTarget.appendChild(backButton);
+    document.getElementById("complete-list").appendChild(completeTarget);
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ(div)を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  // button(戻す)タグ生成
+  const backButton = document.createElement("button");
+  backButton.innerText = "戻す";
+  backButton.addEventListener("click", () => {
+    const backTarget = backButton.parentNode;
+    backTarget.removeChild(backButton);
+    backTarget.appendChild(completeButton);
+    backTarget.appendChild(deleteButton);
+    document.getElementById("incomplete-list").appendChild(backTarget);
+  });
+
   // divタグの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
