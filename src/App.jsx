c29b40815966f20cd8416074ae498fdae86a6a49
Hijikata1869 <liv.knk@gmail.com>
2023-11-08 19:02:40 +0900

    todoの登録数の上限を５個にする機能を実装

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,8 @@ const App = () => {
 
   return (
     <>
-      <InputTodo todoText={todoText} onChange={onChangeTodoText} onClick={onClickAdd} />
+      <InputTodo todoText={todoText} onChange={onChangeTodoText} onClick={onClickAdd} disabled={incompleteTodos.length >= 5} />
+      {incompleteTodos.length >= 5 && <p style={{color: "red"}}>登録できるtodoは5個までです。消化してください。</p>}
       <IncompleteTodos todos={incompleteTodos} onClickComplete={onClickComplete} onClickDelete={onClickDelete} />
       <CompleteTodos todos={completeTodos} onClickBack={onClickBack} />
     </>
